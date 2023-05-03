b8c838e9bad1585fc9d661ed6906d64354abbea0
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-05-03 11:48:49 +0700

    create delete todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-const Todos = ({ todos, todoCompleted }) => {
+const Todos = ({ todos, todoCompleted, deleteTodo }) => {
   return (
     <div style={styles.container}>
       {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} todoCompleted={todoCompleted} />
+        return <TodoItem key={todo.id} todo={todo} todoCompleted={todoCompleted} deleteTodo={deleteTodo} />
       })}
     </div>
   )
